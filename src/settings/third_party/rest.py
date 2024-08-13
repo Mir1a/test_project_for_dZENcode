bad5b81ed9fc38ea305cd392eb1a4104355d35d2
Mir1a <miriamotuss@gmail.com>
2024-08-13 19:12:19 +0300

    fix

--- src/settings/third_party/rest.py
+++ src/settings/third_party/rest.py
@@ -1,12 +1,8 @@
+from datetime import timedelta
+
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
