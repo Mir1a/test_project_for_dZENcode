92e29931937bfdd336090eb969398d776cd1593e
Mir1a <miriamotuss@gmail.com>
2024-08-13 21:48:22 +0300

    fix

--- src/settings/third_party/rest.py
+++ src/settings/third_party/rest.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
